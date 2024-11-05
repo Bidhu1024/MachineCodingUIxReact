89fd0a0fcf412890ac7d312bde2c7115ecf95486
Bidhu1024 <bidhu1024av@gmail.com>
2024-11-05 07:15:06 +0530

    Infinite scrolling added

--- infinitscroll/src/component/InfiniteScroll.tsx
+++ infinitscroll/src/component/InfiniteScroll.tsx
@@ -1,25 +1,28 @@
-import{ useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Post from "./Post";
 
 const InfiniteScroll = () => {
   const [data, setData] = useState<Array<any>>([]);
   const [pageNo, setPageNo] = useState<number>(1);
-console.log(data)
+
+  const memoizedPage = useCallback(()=>{
+setPageNo((page)=>page+1)
+  },[])
+  console.log(data);
   useEffect(() => {
     fetch(`https://picsum.photos/v2/list?page=${pageNo}&limit=5`)
       .then((res) => {
         return res.json();
       })
       .then((d) => {
-        setData((prev)=>[...prev,...d])
-      
+        setData((prev) => [...prev, ...d]);
       })
       .catch((err) => {
         console.log(err);
       });
   }, [pageNo]);
 
-  return <Post data={data} />;
+  return <Post data={data} setPageNo={memoizedPage} />;
 };
 
 export default InfiniteScroll;
--- infinitscroll/src/component/Post.tsx
+++ infinitscroll/src/component/Post.tsx
@@ -1,12 +1,32 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 interface Post{
     data:any;
+    setPageNo: () => void;
 }
-const Post:FC<Post> = ({data}) => {
+const Post:FC<Post> = ({data,setPageNo}) => {
+  useEffect(()=>{
+    const observer = new IntersectionObserver((param)=>{
+console.log(param)
+if(param[0].isIntersecting){
+  observer.unobserve(lastImage);
+setPageNo()
+}
+    })
+const lastImage = document.querySelector('.image-post:last-child')
+if(!lastImage){return}
+observer.observe(lastImage)
+
+return ()=>{
+  if(lastImage){
+    observer.unobserve(lastImage);
+  }
+  observer.disconnect()
+}
+  },[data])
   return (
     <div className='container'>
         {data.map((image)=>{
-  return <img key={image.id} src={image.download_url} alt="dd" />
+  return <img className='image-post' key={image.id} src={image.download_url} alt="dd" />
         })}
            
 
