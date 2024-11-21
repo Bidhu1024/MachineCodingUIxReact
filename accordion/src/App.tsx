772d2e3268b9c8cb6403653a16b3f1c58c4a5601
Bidhu1024 <bidhu1024av@gmail.com>
2024-11-21 10:30:18 +0530

    Accordion completed

--- accordion/src/App.tsx
+++ accordion/src/App.tsx
@@ -1,3 +1,4 @@
+import AccordionList from './components/AccordionList';
 
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
   return (
   <>
   <h1 style={{width:'100%', display:"flex", justifyContent:"center"}}>Accordion</h1>
-  
+  <AccordionList />
   </>
   )
 }
---
+++ accordion/src/components/AccordionList.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+
+const AccordionList = () => {
+
+
+  const [selectedId, setSelectedId] = useState<number | null>(null);
+    const items = [{
+        header:"Item 1",
+        id:1,
+        description :"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+    },{
+        
+            header:"Item 2",
+            id:2,
+            description :"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+        
+    },{
+        header:"Item 3",
+        id:3,
+        description :"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+    },
+    {
+        header:"Item 4",
+        id:4,
+        description :"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+    },
+]
+
+const handleAccordion = (id:number)=>{
+  setSelectedId(prevId => (prevId === id ? null : id));
+}
+  return (
+    <div>
+      {items.map((item)=>{
+        return (
+            <div key={item.id} style={{height:selectedId === item.id?"80px":"50px",border:"1px solid black",padding:"1rem"}}>
+              <div style={{display:"flex", justifyContent:"space-between",}}>
+              <p>{item.header}</p> 
+              <button onClick={()=>{
+                handleAccordion(item.id)
+
+              }}>{selectedId === item.id ? "up":"down"}</button>
+              </div>
+
+              {selectedId === item.id && item.description}
+        
+
+            </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default AccordionList
