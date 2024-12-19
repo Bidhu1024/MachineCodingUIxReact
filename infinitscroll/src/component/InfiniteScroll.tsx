9fd3bb3ff70d1b6a84640babcb5428e1db0cab7c
Bidhu1024 <bidhu1024av@gmail.com>
2024-12-19 08:20:15 +0530

    minor changes

--- infinitscroll/src/component/InfiniteScroll.tsx
+++ infinitscroll/src/component/InfiniteScroll.tsx
@@ -5,9 +5,9 @@ const InfiniteScroll = () => {
   const [data, setData] = useState<Array<any>>([]);
   const [pageNo, setPageNo] = useState<number>(1);
 
-  const memoizedPage = useCallback(()=>{
-setPageNo((page)=>page+1)
-  },[])
+  const memoizedPage = useCallback(() => {
+    setPageNo((page) => page + 1);
+  }, []);
   console.log(data);
   useEffect(() => {
     fetch(`https://picsum.photos/v2/list?page=${pageNo}&limit=5`)
--- infinitscroll/src/component/Post.tsx
+++ infinitscroll/src/component/Post.tsx
@@ -1,37 +1,43 @@
-import React, { FC, useEffect } from 'react'
-interface Post{
-    data:any;
-    setPageNo: () => void;
+import React, { FC, useEffect } from "react";
+interface Post {
+  data: any;
+  setPageNo: () => void;
 }
-const Post:FC<Post> = ({data,setPageNo}) => {
-  useEffect(()=>{
-    const observer = new IntersectionObserver((param)=>{
-console.log(param)
-if(param[0].isIntersecting){
-  observer.unobserve(lastImage);
-setPageNo()
-}
-    })
-const lastImage = document.querySelector('.image-post:last-child')
-if(!lastImage){return}
-observer.observe(lastImage)
+const Post: FC<Post> = ({ data, setPageNo }) => {
+  useEffect(() => {
+    const observer = new IntersectionObserver((param) => {
+      if (param[0].isIntersecting) {
+        observer.unobserve(lastImage);
+        setPageNo();
+      }
+    });
+    const lastImage = document.querySelector(".image-post:last-child");
+    if (!lastImage) {
+      return;
+    }
+    observer.observe(lastImage);
 
-return ()=>{
-  if(lastImage){
-    observer.unobserve(lastImage);
-  }
-  observer.disconnect()
-}
-  },[data])
+    return () => {
+      if (lastImage) {
+        observer.unobserve(lastImage);
+      }
+      observer.disconnect();
+    };
+  }, [data]);
   return (
-    <div className='container'>
-        {data.map((image)=>{
-  return <img className='image-post' key={image.id} src={image.download_url} alt="dd" />
-        })}
-           
-
+    <div className="container">
+      {data.map((image) => {
+        return (
+          <img
+            className="image-post"
+            key={image.id}
+            src={image.download_url}
+            alt="dd"
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
